c459bc7bec17825a6231ba494660d10b327ab3ca
wanghuanqiang <1205549369@qq.com>
2019-05-13 17:56:53 +0800

    提交一个测试类文件 wang

---
+++ javawebv002/src/main/java/com/javaweb/test/TestWang.java
@@ -0,0 +1,19 @@
+package com.javaweb.test;
+
+public class TestWang {
+    public static void main(String[] args) {
+        System.out.println("第一次");
+        System.out.println("第一次");
+        System.out.println("第一次");
+        System.out.println("第一次");
+        System.out.println("第一次");
+
+
+
+
+
+
+
+    }
+
+}
